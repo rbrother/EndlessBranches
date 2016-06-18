10393df859a110d4737be0132f4cec429c088afb
Robert Brotherus <robert@iki.fi>
2016-06-18 19:46:53 +0300

    starting out the making of the jump and now the character roll-around has been fixed

---
+++ Assets/Jump.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp("a")) {
+            print("A has been let up");
+        }
+        else if (Input.GetKeyUp("d")) {
+            print("D has been let up");
+        }
+    }
+}
