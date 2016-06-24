daa0dae3c0df4c0b1dfecb9face5b20511b0c328
Robert Brotherus <robert@iki.fi>
2016-06-24 18:35:01 +0300

    jumping with charging, jumpBar and action box (the green one)

--- Assets/Jump.cs
+++ Assets/Jump.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class Jump : MonoBehaviour {
+
+    float jumpForce = 0;
+    bool barGrowing = false;
 
 	// Use this for initialization
 	void Start () {
@@ -13,13 +17,27 @@ public class Jump : MonoBehaviour {
         var boxCollider = GetComponent<BoxCollider2D>();
         var touching = boxCollider.IsTouchingLayers();
         var rigidBody = GetComponent<Rigidbody2D>();
-        if (Input.GetKeyUp("a") && touching) {
-            rigidBody.velocity = new Vector2(-7, 7);
+        var jumpBar = transform.FindChild("jumpBar");
+        barGrowing = (Input.GetKey("a") || Input.GetKey("d")) && touching;
+
+        if (barGrowing){
+            if (jumpForce <= 100){
+            jumpForce = jumpForce + Time.deltaTime * 200;
+            }
+                     
         }
-        else if (Input.GetKeyUp("d") && touching) {
-            rigidBody.velocity = new Vector2(7, 7);
-            
+
+        if(Input.GetKeyUp("a") || Input.GetKeyUp("d")) {
+            var directionMultiplier = Input.GetKeyUp("a") ? -1 : 1;
+            var ukkoSkaalaus = transform.FindChild("ukko");
+            ukkoSkaalaus.localScale = new Vector3(directionMultiplier, 1);
+            var speed = jumpForce / 100 * 7;
+            rigidBody.velocity = new Vector2(directionMultiplier * speed, speed);
+            jumpForce = 0;
         }
+        jumpBar.localScale = new Vector3(jumpForce / 100 * 45, 1);
     }
 }
+
+//Console used with: Debug.Log(Value or thing to the console);
 
