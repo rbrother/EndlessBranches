7b228ae36dc089804102aeee23493ce63ca3d87f
Robert Brotherus <robert@iki.fi>
2016-07-06 15:34:43 +0300

    start-up screen and the main game connected
    +clicking start at 1st screen executes the kentta1

---
+++ Assets/startScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class startScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown() {
+        Debug.Log("OnMouseDown has been executed!");
+        SceneManager.LoadScene("kentta1");
+    }
+}
