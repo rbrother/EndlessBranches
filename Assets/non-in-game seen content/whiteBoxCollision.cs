d426e7e27018215de98e84ae6ece2f628c3a4c52
Robert Brotherus <robert@iki.fi>
2016-07-13 21:08:30 +0300

    level creation and box collider practice

---
+++ Assets/non-in-game seen content/whiteBoxCollision.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class whiteBoxCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        Debug.Log("Collider triggered! The GameObject's " + coll.gameObject.tag);
+        var whiteBox = transform.FindChild("WhiteBox");
+        var colorProperties = GetComponent<SpriteRenderer>();
+        if (coll.gameObject.tag == "Player") {
+            colorProperties.color = new Color(1, 0, 0);
+        }
+    }
+}
